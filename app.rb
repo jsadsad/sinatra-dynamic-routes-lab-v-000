a036b91dee84f2f770a644c67829047c35d41905
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-25 02:30:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+  
+  get '/square/:number' do
+    @product = params[:number].to_i * params[:number].to_i
+    "#{@product}"
+  end
+  
+  get '/say/:number/:phrase' do
+    i = 0
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    while i < @num
+      puts "#{@phrase}"
+    end
+  end
+    
 end
